b06f4c7029ed0dd038527003ebd8515ac3e4d383
Sheikh Ashequr Rahman <138394916+skashequr@users.noreply.github.com>
2023-12-15 21:18:42 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-const express = require('express');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require("dotenv").config();
-const cors = require('cors');
+const { MongoClient, ServerApiVersion , ObjectId} = require('mongodb');
+const express = require("express");
+var jwt = require('jsonwebtoken');
 
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRYPE_SECRET_KEY);
 const app = express();
 
-const port = process.env.PORT || 5000; 
-// app.use(cors());
-// Middleware 
-app.use(cors()); 
+const port = process.env.PORT || 5000;
+
+const cors = require("cors");
+app.use(
+  cors({
+    origin: ["http://localhost:5173","https://employ-management-20d5c.web.app"],
+    credentials: true,
+  })
+);
+
+// app.use(cors)
 app.use(express.json());
-
-
-console.log(process.env.DB_USER)
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ruyf0su.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -29,166 +32,232 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const servicesItems = client
+    .db("employeemanagement")
+    .collection("services");
+    const usersCollection = client
+    .db("employeemanagement")
+    .collection("usersCollection");
+    const paymentCollection = client
+    .db("employeemanagement")
+    .collection("paymentCollection");
+    const workSheedCollection = client
+    .db("employeemanagement")
+    .collection("workSheedCollection");
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SCRET, { expiresIn: '1h' });
+      res.send({ token });
+    });
+    const verifyToken = (req, res, next) => {
+      // console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+     // use verify admin after verifyToken
+     const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+    app.post("/userInfo", async(req,res)=>{
+      console.log(req.body);
+      const data = req.body;
+      const result = await usersCollection.insertOne(data);
+      return res.send(result);
+    });
+
+
+    // userUpdate/admin
+    app.patch("/userUpdateAdmin", async(req,res)=>{
+      const { value , id} = req.query;
+
+    console.log('Received value:', value , id);
+      const filter = { _id : new ObjectId(id) }
+      const updateDoc = {
+        $set: { selectedRole: value},
+      }
+      const result = await usersCollection.updateOne(filter,updateDoc);
+      res.send(result);
+    });
+    // This data show employ table
+    app.get("/userInfo" , async(req,res)=>{
+      const email = req.query.email;
+      // console.log(email);
+      const users = await usersCollection.find().toArray();
+      return res.send(users)
+    })
+    // Log in users data
+    app.get("/loginUser" , async(req,res)=>{
+      const email = req.query.email;
+      // console.log(email);
+      const filter = { email : email }
+      const users = await usersCollection.find(filter).toArray();
+      return res.send(users)
+    })
+
+    //---------------- Payment Intent ------------
+    app.post("/create-payment-intent", async (req, res) => {
+      try {
+        const { salary } = req.body;
+        console.log(salary);
+        const amount = parseInt(salary * 100);
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount,
+          currency: "usd",
+          payment_method_types: ["card"],
+        });
+    
+        res.send({
+          clientSecret: paymentIntent.client_secret,
+        });
+      } catch (error) {
+        console.error("Error creating payment intent:", error);
+        res.status(500).send({ error: "Error creating payment intent" });
+      }
+    });
+
+    
+    app.post("/payment", async (req, res) => {
+      const payment = req.body;
+      const paymentResult = await paymentCollection.insertOne(payment);
+      res.send(paymentResult);
+    });
+    
+    app.get("/payment", async (req, res) => {
+      const slug = req.query.slug;
+      console.log(slug);
+    
+      const filter = { email : slug }
+      const users = await paymentCollection.find(filter).toArray();
+      return res.send(users)
+    });
+    app.get("/payments", async (req, res) => {
+      const slug = req.query.slug;
+      console.log(slug);
+    
+      const filter = { email : slug }
+      const users = await paymentCollection.find(filter).toArray();
+      return res.send(users)
+    });
+    
+
+
+
+    app.post("/workSheet", async (req, res) => {
+      const payment = req.body;
+      const paymentResult = await workSheedCollection.insertOne(payment);
+      res.send(paymentResult);
+    });
+
+
+
+    app.get("/workSheet", async (req, res) => {
+      const filter = { email: req.query.email }; 
+      console.log(filter);
+    
+      try {
+        const paymentResult = await workSheedCollection.find(filter).toArray();
+        res.send(paymentResult);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+
+   app.get('/workSheetPagination', async (req, res) => {
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      const filter = { email: req.query.email };
+      console.log('pagination query worksheet pagination', page, size, filter);
+      const result = await workSheedCollection.find(filter)
+        // .skip(page * size)
+        .limit(size)
+        .toArray();
+      res.send(result);
+    });
+
+app.get('/workSheetHrPagination', async (req, res) => {
+  const page = parseInt(req.query.page);
+  const size = parseInt(req.query.size);
+  console.log('pagination query', page, size);
+  const result = await workSheedCollection.find()
+  .skip(page * size)
+  .limit(size)
+  .toArray();
+  res.send(result);
+  
+})
+    app.get("/workSheetCount", async (req, res) => {
+      
+      try {
+        const count = await workSheedCollection.estimatedDocumentCount();
+        // const count2 = await workSheedCollection.find().estimatedDocumentCount();
+        res.send({count});
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+
+
+
+
+
+
+
+
+    app.patch("/usersInfo/admin/:id" , async(req,res)=>{
+      const id = req.params.id
+      console.log(id);
+      const filter = { _id : new ObjectId(id) }
+      const updateDoc = {
+        $set: { veryfi: true},
+      }
+      const result = await usersCollection.updateOne(filter,updateDoc);
+      console.log(result);
+      res.send(result);
+    })
+    app.get("/services", async (req, res) => {
+        try {
+            const services = await servicesItems.find().toArray();
+            return res.send(services);
+          } catch (error) {
+            res.status(500).send("Internal Server Error: " + error.message);
+          }
+      });
+      app.get("/services-Details", async (req, res) => {
+        try {
+          const title = req.query.title;
+          console.log(title);
+  
+          const filter = { title: title };
+          const result = await servicesItems.findOne(filter)
+          return res.send(result);
+        } catch (error) {
+            console.error("Error fetching service details:", error);
+            res.status(500).send("Internal Server Error: " + error.message);
+        }
+    });
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
-    const productCollection = client.db("myDB").collection("productsDB");
-    const categoriesCollection = client.db("myDB").collection("categorieDB");
-    const userAddedDataCollection = client.db("myDB").collection("userAddedData");
-    app.use(express.json());
-
-    app.post("/product", async (req, res) => {
-      const newProduct = req.body;
-      const result = await productCollection.insertOne(newProduct);
-      res.send(result);
-      console.log(newProduct);
-    });
-
-
-    app.post("/myproducts",async(req,res)=>{
-      const addedProudects = req.body;
-      console.log(addedProudects);
-      const result = await userAddedDataCollection.insertOne(addedProudects);
-      res.send(result)
-      
-    })
-    
-
-    // app.get("/myproducts", async (req, res) => {
-    //   const email = req.query.email;
-    //   const addedProudects = req.body;
-    //   // console.log(addedProudects);
-    //     // Perform a find operation to get data by email
-    //     const query = { email: email };
-    //     const result = await userAddedDataCollection.find(query).toArray();
-    
-    //     res.send(result);
-     
-    // });
-
-    app.post('/myproducts', async (req, res) => {
-      const addedProduct = req.body;
-      const userEmail = user.email; // Assuming you have the user's email from authentication
-      addedProduct.email = userEmail; // Add the user's email to the data
-      const result = await userAddedDataCollection.insertOne(addedProduct);
-      res.send(result);
-    });
-    
-    app.get('/myCard/:email', async (req, res) => {
-      try {
-        const userEmail = req.params.email; // User provides their email in the URL
-        const query = { email: userEmail };
-        const result = await userAddedDataCollection.find(query).toArray();
-        res.json(result);
-      } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-      }
-    });
-    
-
-
-    app.get("/product",async (req,res)=>{
-      const cursor = productCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    })
-
-    app.post("/categorie", async (req, res) => {
-      const newCategory = req.body;
-      const result = await categoriesCollection.insertOne(newCategory);
-      res.send(result);
-      console.log(newCategory);
-    });
-
-
-    app.get("/categorie",async (req,res)=>{
-      const cursor = categoriesCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    })
-
-    app.get("/:brandName", async (req, res) => {
-      const brandName = req.params.brandName;
-   
-      const query = { brandName };
-    
-      try {
-        const result = await productCollection.find(query).toArray();
-    
-        if (result.length === 0) {
-          res.status(404).send("Category not found");
-        } else {
-          res.send(result);
-        }
-      } catch (error) {
-        res.status(500).send("An error occurred: " + error.message);
-      }
-    });
-    
-
-    app.get("/:brandName/:details", async (req, res) => {
-      const details = req.params.details;
-      const query = { name: details }; // Use a string-based query
-      try {
-        const result = await productCollection.find(query).toArray();
-        res.send(result);
-      } catch (error) {
-        res.status(500).send("Internal Server Error: " + error.message);
-      }
-    });
-    
-    app.get("/:brandName/:details/:update", async (req, res) => {
-      try {
-        const details = req.params.details;
-        const query = { name: details };
-        const result = await productCollection.find(query).toArray();
-        res.send(result);
-      } catch (error) {
-        console.error("Error in database query:", error);
-        res.status(500).send("Internal Server Error");
-      }
-    });
-    
-
-    app.put('/id/:iddetails/:update', async (req, res) => {
-      const update = req.params.update;
-      const filter = { _id: new ObjectId(update) };
-      const updatedData = req.body;
-      const updated = {
-          $set: {
-              name: updatedData.name,
-              brandName: updatedData.brandName,
-              price: updatedData.price,
-              shortDescription: updatedData.shortDescription,
-              image: updatedData.image,
-              rating: updatedData.rating
-          }
-      };
-  
-      try {
-          const result = await productCollection.updateOne(filter, updated);
-          res.json(result);
-      } catch (error) {
-          console.error(error);
-          res.status(500).json({ error: 'Internal Server Error' });
-      }
-  });
-  
-
-
-    app.delete("/myCart/:email/:id", async(req,res)=>{
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await userAddedDataCollection.deleteOne(query);
-      res.send(result)
-    })
-
-    
-    
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -196,13 +265,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-    res.send('Assignment Ten Running');
-});
-
-app.listen(port, () => {
-    console.log(`Assignment ten server is running on port ${port}`);
-});
+app.get("/", (req, res) => {
+    res.send("Employee Management Services...");
+  });
+  
+  app.listen(port, () => {
+    console.log(`Employee Management Services is Running on port ${port}`);
+  });
